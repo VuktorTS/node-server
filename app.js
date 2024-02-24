9b014ac7b0fb96b8507f60563dc16987b7e3a693
serhii.tsykalovskyi <serhii.tsykalovskyi@paysera.com>
2024-02-24 20:05:13 +0200

    add simple functionality

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 import http from 'http';
+import fs from 'fs';
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
+  var url = req.url;
+  if (url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+  } else if(url === '/test') {
+    fs.readFile("views/test.html", function (err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+      res.write(data);
+      res.end();
+    })
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Content for '+url+' url was not provided!');
+  }
 });
 
 app.listen(port, hostname, () => {
